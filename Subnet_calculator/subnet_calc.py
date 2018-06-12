1c6464f4ac6dd42b6b6c5431f76d918aad21c4d4
Vova <ovod88@bigmir.net>
2018-06-12 21:13:23 +0000

    Added convertor mask presented by number to 255 form

--- Subnet_calculator/subnet_calc.py
+++ Subnet_calculator/subnet_calc.py
@@ -1,4 +1,15 @@
 import sys
+import linecache
+import traceback
+
+def printException():
+	exc_type, exc_obj, tb = sys.exc_info()
+	f = tb.tb_frame
+	lineno = tb.tb_lineno
+	filename = f.f_code.co_filename
+	linecache.checkcache(filename)
+	line = linecache.getline(filename, lineno, f.f_globals)
+	print(f'EXCEPTION IN ({filename}, LINE {lineno} "{line.strip()}": {exc_obj})')
 
 def if_valid_ip(ip):
 	ip_addr_octets = ip.split('.')
@@ -11,41 +22,73 @@ def if_valid_ip(ip):
 		and first_octet != 127 and (first_octet != 169 or second_octet != 254) \
 		and (0 <= second_octet <= 255 and 0 <= third_octet <= 255 and 0 <= last_octet <= 255)
 
-
 def if_valid_mask(mask):
 	mask_octets = mask.split('.')
-	
-	if len(mask_octets) == 1 and 1 <= mask_octets[0] <= 32:
+	masks = [255, 254, 252, 248, 240, 224, 192, 128, 0]
+
+
+	if len(mask_octets) == 1 and 1 <= int(mask_octets[0]) <= 32:
 		return True
-	elif 
+	elif len(mask_octets) == 4 and (int(mask_octets[0]) in masks and int(mask_octets[1]) in masks \
+							and int(mask_octets[2]) in masks and int(mask_octets[3]) in masks \
+							and (int(mask_octets[0]) >= int(mask_octets[1]) >= int(mask_octets[2]) >= int(mask_octets[3]))):
+		return True
+	else:
+		return False
+
+def convertMaskToBinaryTuple(maskString):
+	mask_octets = maskString.split('.')
+	mask_to_return = []
+
+	if len(mask_octets) == 1:
+		mask_bits = int(mask_octets[0])
+		
+		while mask_bits > 8:
+			mask_to_return.append(2 ** 8 - 1)
+			mask_bits -= 8
+
+		mask_to_return.append(2 ** 8 - 2 ** (8 - mask_bits))
+		while len(mask_to_return) < 4:
+			mask_to_return.append(0)
+	else:
+		pass
+
+
+
+	return tuple(mask_to_return)
+
 
 def subnet_calc():
 	try:
-		while True:
-			ip_addr = input('Enter an IP address: ')
+		# while True:
+		# 	ip_addr = input('Enter an IP address: ')
 
-			if if_valid_ip(ip_addr):
-				print(f'Ip address {ip_addr} is valid')
-				break
-			else:
-				print('\nThe IP address is INVALID. Retry please!\n')
+		# 	if if_valid_ip(ip_addr):
+		# 		print(f'Ip address {ip_addr} is valid')
+		# 		break
+		# 	else:
+		# 		print('\nThe IP address is INVALID. Retry please!\n')
 
-		# print(f'Ip address {ip_addr} is valid')
 		while True:
 			subnet_mask = input('Enter a subnet mask: ')
 
 			if if_valid_mask(subnet_mask):
 				print(f'Mask {subnet_mask} is valid')
-				break
+				mask_binary_octets = convertMaskToBinaryTuple(subnet_mask)
+				print(mask_binary_octets)
+				# break
 			else:
 				print('\nThe mask is INVALID. Retry please!\n')
-			
+
+		mask_binary_octets = convertMaskToBinaryTuple(subnet_mask)
+		print(mask_binary_octets)
 
 	except KeyboardInterrupt:
 		print('\n\nProgram aborted by user. Exitting...\n')
 		sys.exit()
 	except Exception as e:
-		print('Error happened -> ' + str(e))
-
+		# printException()
+		exc_info = sys.exc_info()
+		traceback.print_exception(*exc_info)
 
 subnet_calc()
